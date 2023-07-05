a633e1e28a8b05f5a8351d622b7e09f9e663fec4
smsrezaei <67260434+smsrezaei@users.noreply.github.com>
2023-07-05 12:10:22 +0330

    .
    .

---
+++ StockDBUpdate/StockDBUpdate/EF/Type1Stock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StockDBUpdate.EF
+{
+    public partial class Type1Stock
+    {
+        public long Id { get; set; }
+        public string Symbol { get; set; } = null!;
+        public long? InsCode { get; set; }
+        public decimal? LastPrice { get; set; }
+        public decimal? SupportPrice { get; set; }
+        public decimal? TargetPrice1 { get; set; }
+        public decimal? TargetPrice2 { get; set; }
+        public decimal? TargetPrice3 { get; set; }
+        public DateTime Tmst { get; set; }
+    }
+}
